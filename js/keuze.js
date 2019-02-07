869c9e0afdf62d4e38ca0f286e88bf76074d17bb
JeannotM <42801349+JeannotM@users.noreply.github.com>
2019-02-07 13:07:17 +0100

    Begin UI
    Heeft veel van de ui elementen

---
+++ js/keuze.js
@@ -0,0 +1,9 @@
+let a = array("divOverOns", "divWaaromOns", "divTemp")
+let main = document.getElementsByClassName("main");
+function main(id, className){
+    // let b = className;
+    // main.className=b;
+    // document.getElementById(id).className="main";
+    console.log(id);
+    console.log(className);
+}
